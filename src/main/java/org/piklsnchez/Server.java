99494705e9496bdef8bf62ed11d7710639b2b8ca
Chris DuBose <chris.dubose@swgas.com>
2017-10-19 16:31:00 -0700

    remove rest

--- src/main/java/org/piklsnchez/Server.java
+++ src/main/java/org/piklsnchez/Server.java
@@ -1,32 +1,62 @@
 package org.piklsnchez;
 
-import java.util.stream.Stream;
-import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 import java.util.logging.Logger;
-import java.net.URI;
-import org.glassfish.jersey.server.ResourceConfig;
-import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
-import org.glassfish.grizzly.http.server.StaticHttpHandler;
+import java.util.stream.Stream;
 import org.glassfish.grizzly.http.server.HttpServer;
-import org.glassfish.grizzly.http.server.HttpHandler;
-import org.glassfish.grizzly.http.server.Request;
-import org.glassfish.grizzly.http.server.Response;
+import org.glassfish.grizzly.websockets.DataFrame;
+import org.glassfish.grizzly.websockets.WebSocket;
+import org.glassfish.grizzly.websockets.WebSocketAddOn;
+import org.glassfish.grizzly.websockets.WebSocketApplication;
+import org.glassfish.grizzly.websockets.WebSocketEngine;
 
 public class Server {
     private static final String CLASS = Server.class.getName();
     private static final Logger LOG = Logger.getLogger(CLASS);
     
-    private static final String BASE_URI = "http://0.0.0.0:8080/";
-    
     public static void main(String... args){
-        ResourceConfig resourceConfig = new ResourceConfig().packages("org.piklsnchez.rest");
-        HttpServer server = GrizzlyHttpServerFactory.createHttpServer(URI.create(BASE_URI), resourceConfig);
-        server.getServerConfiguration().addHttpHandler(new StaticHttpHandler("."), "/static");
+        HttpServer server = HttpServer.createSimpleServer(); 
+        //server.getServerConfiguration().addHttpHandler(new StaticHttpHandler("."), "/static");
+        server.getListener("grizzly").registerAddOn(new WebSocketAddOn());
+        WebSocketEngine.getEngine().register("/ws", "/event", 
+            new WebSocketApplication() {
+                private String offer  = "{}";
+                private String answer = "{}";
+                private List<WebSocket> sockets = new ArrayList<>();
+                
+                @Override
+                public void onConnect(WebSocket ws){
+                    LOG.entering(getClass().getName(), "onConnect", ws);
+                    sockets.add(ws);
+                    LOG.exiting(CLASS, "onConnect");
+                }
+                @Override
+                public void onClose(WebSocket ws, DataFrame df){
+                    LOG.entering(getClass().getName(), "onClose", Stream.of(ws, df).toArray());
+                    sockets.remove(ws);
+                    LOG.exiting(CLASS, "onClose");
+                }
+                @Override
+                public void onMessage(WebSocket ws, String data){
+                    LOG.entering(getClass().getName(), "onMessage", Stream.of(ws, data).toArray());
+                    if(data.contains("\"offer\":")){
+                        this.offer = data;
+                        sockets.parallelStream().filter(w -> !Objects.equals(w, ws)).forEach(w -> w.send(this.offer));
+                    } else if(data.contains("\"answer\":")){
+                        this.answer = data;
+                        sockets.parallelStream().filter(w -> !Objects.equals(w, ws)).forEach(w -> w.send(this.answer));
+                    }
+                    LOG.exiting(CLASS, "onMessage");
+                }
+            }
+        );
         try {
             server.start();
             System.out.println("Press any key to stop the server...");
             System.in.read();
-            server.stop();
+            server.shutdownNow();
         } catch (Exception e) {
             LOG.severe(e.toString());
         }
--- src/main/java/org/piklsnchez/rest/Rest.java
+++ src/main/java/org/piklsnchez/rest/Rest.java
@@ -1,18 +1,12 @@
 package org.piklsnchez.rest;
 
 import java.util.logging.Logger;
-import java.util.List;
-import java.util.ArrayList;
 import javax.inject.Singleton;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Context;
-import javax.ws.rs.sse.Sse;
-import javax.ws.rs.sse.SseEventSink;
-import javax.ws.rs.sse.OutboundSseEvent;
 
 @Singleton
 @Path("/rest")
@@ -22,20 +16,7 @@ public class Rest {
     
     private String offer  = "{}";
     private String answer = "{}";
-    private final Sse sse;
-    private final List<SseEventSink> sinkers;
     
-    public Rest(@Context final Sse sse){
-        this.sse = sse;
-        sinkers  = new ArrayList<>();
-    }
-    
-    @GET
-    @Path("event")
-    @Produces(MediaType.SERVER_SENT_EVENTS)
-    public void getEvents(@Context SseEventSink eventSink){
-        sinkers.add(eventSink);
-    }
     
     @GET
     @Path("offer")
@@ -49,7 +30,6 @@ public class Rest {
     public String setOffer(String body){
         LOG.entering(CLASS, "setOffer", body);
         offer = body;
-        sinkers.forEach(s -> s.send(sse.newEventBuilder().name("offer").data(String.class, offer).build()));
         LOG.exiting(CLASS, "setOffer");
         return null;
     }
@@ -68,7 +48,6 @@ public class Rest {
     public String setAnswer(String body){
         LOG.entering(CLASS, "setAnswer", body);
         answer = body;
-        sinkers.forEach(s -> s.send(sse.newEventBuilder().name("answer").data(String.class, answer).build()));
         LOG.exiting(CLASS, "setAnswer");
         return null;
     }
